7e8c11d63892e7f8ce44ec7f4421743e2fea158b
KreativeThinker <sagely2005eagle@gmail.com>
2025-06-04 16:12:52 +0530

    feat: load from config.yaml

--- config.py
+++ config.py
@@ -1,19 +1,24 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 
 @dataclass
 class Config:
-    input_folder: str
-    output_folder: str
+    data_dir: Path
+    raw_data_dir: Path
+    input_dir: Path
+    output_dir: Path
     number_of_animals: int
     verbose: bool
     time_chunks: int
 
+    @staticmethod
+    def from_yaml(yaml_path: str) -> "Config":
+        import yaml
 
-config = Config(
-    input_folder="data/input",
-    output_folder="data/output",
-    number_of_animals=7,
-    verbose=True,
-    time_chunks=3600,  # 1 Hour chunks
-)
+        with open(yaml_path, "r") as file:
+            data = yaml.safe_load(file)
+        return Config(**data)
+
+
+config = Config.from_yaml("config.yaml")
