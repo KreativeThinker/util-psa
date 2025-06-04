b8b23911d75e835db28de02f1c0201aa7b258cda
KreativeThinker <sagely2005eagle@gmail.com>
2025-06-04 12:44:53 +0530

    feat: config setup

---
+++ config.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    input_folder: str
+    output_folder: str
+    number_of_animals: int
+    verbose: bool
+
+
+config = Config(
+    input_folder="data/input",
+    output_folder="data/output",
+    number_of_animals=7,
+    verbose=True,
+)
